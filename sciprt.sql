8880ca194832c66ecc23c909d91bb8be96b0e52c
TheuZCoder <matheusrdsilva1@gmail.com>
2024-04-19 14:28:05 -0300

    Atualizações Script

---
+++ sciprt.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE AT5;
+
+CREATE TABLE TabelaEX(
+    NOME VARCHAR (100) NULL
+);
+
+CREATE TABLE TabelaFX(
+    NOME VARCHAR (100) NULL
+);
+
+INSERT INTO TabelaEX 
+VALUES('claudia'),('Cadu'),('Bruno'),('Lucineia'),('Augusto'
+
+INSERT INTO TabelaFX)
+VALUES('Amarildo'),('Alvaro'),('Bruno'),('Damares'),('Augusto')
+
